d25101b666ae3470d0023b07e18348e8dfe53cc7
ahmadsshehri <ahmad.s.shehri@gmail.com>
2026-04-30 04:33:20 +0300

    Update page.tsx

--- app/cashflow/page.tsx
+++ app/cashflow/page.tsx
@@ -168,7 +168,7 @@ export default function CashflowPage() {
       setAppUser(user);
       const props = await loadPropertiesForUser(fbUser.uid, user.role);
       setProperties(props);
-     if (props.length > 0) {
+   if (props.length > 0) {
   const savedId = localStorage.getItem('selectedPropertyId');
   const saved = props.find(p => p.id === savedId);
   const selected = saved || props[0];
